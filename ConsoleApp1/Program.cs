3f96857df82a33a8decba1dff87cfccc0655f9df
Mohamed Hamada <moha2016226@gmail.com>
2024-10-17 02:21:52 -0700

    invoke the stackExample also in main method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //ExecuteStackExample();
+            ExecuteStackExample();
             ExecuteQueueExample();
         }
         static void ExecuteQueueExample()
